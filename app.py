911449d9f38acc603ba2feb96f43020732a423d3
Ayushh45 <108990245+Ayushh45@users.noreply.github.com>
2025-03-23 15:48:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 #from tft_sepsiiss import *  # Import your model and functions
 
 # --- Page Configuration ---
